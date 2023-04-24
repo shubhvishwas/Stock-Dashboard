269e02d11bdffcd87c99b078314b151aa235d901
Shubh vishwas <37065758+shubhvishwas@users.noreply.github.com>
2023-04-24 14:16:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,96 +27,96 @@ with Cnadlestick:
   df=data
 # removing all empty dates
 # build complete timeline from start date to end date
-dt_all = pd.date_range(start=df.index[0],end=df.index[-1])
-# retrieve the dates that ARE in the original datset
-dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(df.index)]
-# define dates with missing values
-dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
-fig.update_layout(xaxis_rangebreaks=[dict(values=dt_breaks)])
-# add moving averages to df
-df['MA20'] = df['Close'].rolling(window=20).mean()
-df['MA5'] = df['Close'].rolling(window=5).mean()
-# first declare an empty figure
-fig = go.Figure()
-# add OHLC trace
-macd = MACD(close=df['Close'], 
-            window_slow=26,
-            window_fast=12, 
-            window_sign=9)
-# stochastics
-stoch = StochasticOscillator(high=df['High'],
-                             close=df['Close'],
-                             low=df['Low'],
-                             window=14, 
-                             smooth_window=3)
+  dt_all = pd.date_range(start=df.index[0],end=df.index[-1])
+  # retrieve the dates that ARE in the original datset
+  dt_obs = [d.strftime("%Y-%m-%d") for d in pd.to_datetime(df.index)]
+  # define dates with missing values
+  dt_breaks = [d for d in dt_all.strftime("%Y-%m-%d").tolist() if not d in dt_obs]
+  fig.update_layout(xaxis_rangebreaks=[dict(values=dt_breaks)])
+  # add moving averages to df
+  df['MA20'] = df['Close'].rolling(window=20).mean()
+  df['MA5'] = df['Close'].rolling(window=5).mean()
+  # first declare an empty figure
+  fig = go.Figure()
+  # add OHLC trace
+  macd = MACD(close=df['Close'], 
+              window_slow=26,
+              window_fast=12, 
+              window_sign=9)
+  # stochastics
+  stoch = StochasticOscillator(high=df['High'],
+                               close=df['Close'],
+                               low=df['Low'],
+                               window=14, 
+                               smooth_window=3)
 
-fig = make_subplots(rows=4, cols=1, shared_xaxes=True,
-                    vertical_spacing=0.01, 
-                    row_heights=[0.5,0.1,0.2,0.2])
-# Plot OHLC on 1st subplot (using the codes from before)
-fig.add_trace(go.Candlestick(x=df.index,
-                             open=df['Open'],
-                             high=df['High'],
-                             low=df['Low'],
-                             close=df['Close'], 
-                             showlegend=False))
-# add moving average traces
-fig.add_trace(go.Scatter(x=df.index, 
-                         y=df['MA5'], 
-                         line=dict(color='blue', width=2), 
-                         name='MA 5'))
-fig.add_trace(go.Scatter(x=df.index, 
-                         y=df['MA20'], 
-                         line=dict(color='orange', width=2), 
-                         name='MA 20'))
+  fig = make_subplots(rows=4, cols=1, shared_xaxes=True,
+                      vertical_spacing=0.01, 
+                      row_heights=[0.5,0.1,0.2,0.2])
+  # Plot OHLC on 1st subplot (using the codes from before)
+  fig.add_trace(go.Candlestick(x=df.index,
+                               open=df['Open'],
+                               high=df['High'],
+                               low=df['Low'],
+                               close=df['Close'], 
+                               showlegend=False))
+  # add moving average traces
+  fig.add_trace(go.Scatter(x=df.index, 
+                           y=df['MA5'], 
+                           line=dict(color='blue', width=2), 
+                           name='MA 5'))
+  fig.add_trace(go.Scatter(x=df.index, 
+                           y=df['MA20'], 
+                           line=dict(color='orange', width=2), 
+                           name='MA 20'))
 
-# Plot volume trace on 2nd row 
-colors = ['green' if row['Open'] - row['Close'] >= 0 
-          else 'red' for index, row in df.iterrows()]
-fig.add_trace(go.Bar(x=df.index, 
-                     y=df['Volume'],
-                     marker_color=colors
-                    ), row=2, col=1)
+  # Plot volume trace on 2nd row 
+  colors = ['green' if row['Open'] - row['Close'] >= 0 
+            else 'red' for index, row in df.iterrows()]
+  fig.add_trace(go.Bar(x=df.index, 
+                       y=df['Volume'],
+                       marker_color=colors
+                      ), row=2, col=1)
 
-# Plot MACD trace on 3rd row
-colors = ['green' if val >= 0 
-          else 'red' for val in macd.macd_diff()]
-fig.add_trace(go.Bar(x=df.index, 
-                     y=macd.macd_diff(),
-                     marker_color=colors
-                    ), row=3, col=1)
-fig.add_trace(go.Scatter(x=df.index,
-                         y=macd.macd(),
-                         line=dict(color='red', width=2)
-                        ), row=3, col=1)
-fig.add_trace(go.Scatter(x=df.index,
-                         y=macd.macd_signal(),
-                         line=dict(color='blue', width=1)
-                        ), row=3, col=1)
+  # Plot MACD trace on 3rd row
+  colors = ['green' if val >= 0 
+            else 'red' for val in macd.macd_diff()]
+  fig.add_trace(go.Bar(x=df.index, 
+                       y=macd.macd_diff(),
+                       marker_color=colors
+                      ), row=3, col=1)
+  fig.add_trace(go.Scatter(x=df.index,
+                           y=macd.macd(),
+                           line=dict(color='red', width=2)
+                          ), row=3, col=1)
+  fig.add_trace(go.Scatter(x=df.index,
+                           y=macd.macd_signal(),
+                           line=dict(color='blue', width=1)
+                          ), row=3, col=1)
 
-# Plot stochastics trace on 4th row 
-fig.add_trace(go.Scatter(x=df.index,
-                         y=stoch.stoch(),
-                         line=dict(color='orange', width=2)
-                        ), row=4, col=1)
-fig.add_trace(go.Scatter(x=df.index,
-                         y=stoch.stoch_signal(),
-                         line=dict(color='blue', width=1)
-                        ), row=4, col=1)
+  # Plot stochastics trace on 4th row 
+  fig.add_trace(go.Scatter(x=df.index,
+                           y=stoch.stoch(),
+                           line=dict(color='orange', width=2)
+                          ), row=4, col=1)
+  fig.add_trace(go.Scatter(x=df.index,
+                           y=stoch.stoch_signal(),
+                           line=dict(color='blue', width=1)
+                          ), row=4, col=1)
 
-# update layout by changing the plot size, hiding legends & rangeslider, and removing gaps between dates
-fig.update_layout(height=900, 
-                  showlegend=False, 
-                  xaxis_rangeslider_visible=False,
-                  xaxis_rangebreaks=[dict(values=dt_breaks)])
-fig.update_layout(title=ticker)
+  # update layout by changing the plot size, hiding legends & rangeslider, and removing gaps between dates
+  fig.update_layout(height=900, 
+                    showlegend=False, 
+                    xaxis_rangeslider_visible=False,
+                    xaxis_rangebreaks=[dict(values=dt_breaks)])
+  fig.update_layout(title=ticker)
 
-# update y-axis label
-fig.update_yaxes(title_text="Price", row=1, col=1)
-fig.update_yaxes(title_text="Volume", row=2, col=1)
-fig.update_yaxes(title_text="MACD", showgrid=False, row=3, col=1)
-fig.update_yaxes(title_text="Stoch", row=4, col=1)
-st.plotly_chart(fig)
+  # update y-axis label
+  fig.update_yaxes(title_text="Price", row=1, col=1)
+  fig.update_yaxes(title_text="Volume", row=2, col=1)
+  fig.update_yaxes(title_text="MACD", showgrid=False, row=3, col=1)
+  fig.update_yaxes(title_text="Stoch", row=4, col=1)
+  st.plotly_chart(fig)
   
 pricing_data,fundamental_data,news,tech_indicator=st.tabs(["Pricing Data","Fundamental Data","Top 10 News","Technical Analysis Dashboard"])
 
